8a22afccb426daed811079442de1e0df19736292
EduSantosBrito <edu.santos.brito@gmail.com>
2022-11-18 12:02:03 +0100

    chore: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
@@ -9,5 +11,6 @@ export default defineConfig({
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
+    sitemap(),
   ],
 });
